c7307750cac83b5f27ad0fd82d21b9f71bd6c0fb
Bhavesh Chavhan <97518972+Bhavesh299@users.noreply.github.com>
2022-06-14 21:20:35 +0530

    Add files via upload

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,15 +4,45 @@ import { fireEvent, render, screen, within } from "@testing-library/react";
 import App from "../App";
 
 describe("Testing routing Application", () => {
+  test("renders the counter value", () => {
+    render(<App />);
+    expect(screen.getByTestId("counter-value")).toHaveTextContent("0");
+  }
+  );
+  test("renders the decrement button", () => {
+    render(<App />);
+    expect(screen.getByTestId("counter-decrement-button")).toBeInTheDocument();
+  }
+  );
+  test("renders the increment button", () => {
+    render(<App />);
+    expect(screen.getByTestId("counter-increment-button")).toBeInTheDocument();
+  }
+  );
+  
   beforeAll(() => {
-    global.score = 0;
-    console.log("Resetting Score to 0");
-  });
+    global.score = 0 ;
+    console.log("Resetting Score to 0") ;
 
+  });
+  test("clicks the decrement button", () => {
+    render(<App />);
+    fireEvent.click(screen.getByTestId("counter-decrement-button"));
+    expect(screen.getByTestId("counter-value")).toHaveTextContent("-1");
+  }
+  );
+  test("clicks the increment button", () => {
+    render(<App />);
+    fireEvent.click(screen.getByTestId("counter-increment-button"));
+    expect(screen.getByTestId("counter-value")).toHaveTextContent("1");
+  }
+  );
   describe("App component should", () => {
     beforeEach(() => {
       render(<App />);
+
     });
+
 
     it("render App and check structure", async () => {
       expect(screen.getByTestId("counter-value")).toBeInTheDocument();
@@ -31,22 +61,25 @@ describe("Testing routing Application", () => {
     });
 
     it("check increment/decrement function", () => {
-      let incButton = screen.getByTestId("counter-increment-button");
-      let decButton = screen.getByTestId("counter-decrement-button");
+      let incButton = screen.getByTestId("counter-increment-button") ;
+      let decButton = screen.getByTestId("counter-decrement-button") ;
 
       // clicked 3 times
       fireEvent.click(incButton);
       fireEvent.click(incButton);
       fireEvent.click(incButton);
-      expect(screen.getByTestId("counter-value")).toHaveTextContent(3);
+
+      expect(screen.getByTestId("counter-value")).toHaveTextContent(3) ;
+
       global.score += 2;
+      
 
       expect(decButton).not.toBeDisabled();
       // clicked 3 times
       fireEvent.click(decButton);
       fireEvent.click(decButton);
       fireEvent.click(decButton);
-      expect(screen.getByTestId("counter-value")).toHaveTextContent(0);
+      expect(screen.getByTestId("counter-value")).toHaveTextContent(0) ;
       global.score += 2;
 
       expect(decButton).toBeDisabled();
@@ -54,7 +87,10 @@ describe("Testing routing Application", () => {
     });
   });
 
+
   afterAll(() => {
+    console.log("Score: " + global.score);
     console.log("Final Score is", global.score);
   });
 });
+
